f61ba931ee09cdc2b92944f76c8981cf18f74617
davidtom <davidjtomczyk@gmail.com>
2017-08-13 21:25:20 -0400

    set up infinite scroll

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -3,7 +3,7 @@ import {PageHeader} from "./Headers";
 import HighlightCollection from "./HighlightCollection";
 import Highlight from "./Highlight";
 import { Container, Header } from 'semantic-ui-react'
-
+import InfiniteScroll from "react-infinite-scroller"
 
 class IndexPage extends React.Component{
 
@@ -12,25 +12,56 @@ class IndexPage extends React.Component{
 
     this.state = {
       highlights: [],
+      hasMoreItems: true,
+      nextPage: 1
     }
 
   }
 
-  getHighlights(){
-    return fetch("http://localhost:3000/api/v1/highlights")
+  loadHighlights = (page) => {
+    let url = "http://localhost:3000/api/v1/highlights?page="
+
+    if (this.state.nextPage){
+      url += this.state.nextPage
+    }
+
+    fetch(url)
     .then(resp => resp.json())
-    .then(highlights => this.setState({highlights}))
+    .then(highlight_page => {
+      if (highlight_page.length){
+        this.setState({
+          highlights: [...this.state.highlights, ...highlight_page],
+          nextPage: this.state.nextPage + 1
+        })
+
+
+      } else {
+        this.setState({
+          hasMoreItems: false
+        })
+      }
+    })
+
   }
 
-  componentDidMount(){
-    this.getHighlights()
-  }
+  // TODO: Set up a loader!
 
   render(){
+
+    const loader = <div className="loader">Loading ...</div>;
+
     return(
       <Container textAlign="center">
         <PageHeader title="Highlights"/>
-        <HighlightCollection highlights = {this.state.highlights} />
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.loadHighlights}
+          hasMore={this.state.hasMoreItems}
+          loader={loader}>
+
+            <HighlightCollection highlights = {this.state.highlights} />
+
+        </InfiniteScroll>
       </Container>
     )
   }
